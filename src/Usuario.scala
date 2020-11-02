d672a00a3aa3ea98ceee5c4ee223879d4d24b4a8
María Jesús López Salmerón <mjls130598@gmail.com>
2020-11-02 17:58:58 +0100

    #10 Modificada la clase Usuario

--- src/Usuario.scala
+++ src/Usuario.scala
@@ -7,21 +7,4 @@ class Usuario(nom: String, email: String, password : String, car: String, uni: S
   val contrasenia: String = password
   val carrera: String = car
   val universidad: String = uni
-
-  def getNombre(): String = {
-    return nombre
-  }
-
-  def getCorreo(): String = {
-    return correo
-  }
-
-  def getCarrera(): String = {
-    return carrera
-  }
-
-  def getUniversidad(): String = {
-    return universidad
-  }
-
 }
