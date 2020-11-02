fd0e1c5ef882e89d2d4335bac7a62fbbd5120a96
María Jesús López Salmerón <mjls130598@gmail.com>
2020-11-02 17:51:03 +0100

    #12, #14, #15, #16 Modificado Apuntes

--- src/Apuntes.scala
+++ src/Apuntes.scala
@@ -5,17 +5,5 @@ class Apunte(nom: String, asig: Asignatura, us: Usuario){
   val nombre:String = nom
   val asignatura: Asignatura = asig
   val usuario: Usuario = us
-
-  def getNombre(): String = {
-    return nombre
-  }
-
-  def getAsignatura(): Asignatura = {
-    return asignatura
-  }
-
-  def getUsuario(): Usuario = {
-    return usuario
-  }
 }
 
