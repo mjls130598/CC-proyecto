6de08c7b6e94b45fec24a72494c3485944b32895
María Jesús López Salmerón <mjls130598@gmail.com>
2020-11-02 18:02:32 +0100

    #18, #19 Modificada la clase Asignatura

--- src/Asignatura.scala
+++ src/Asignatura.scala
@@ -5,20 +5,4 @@ class Asignatura (nom: String, cur: Int, car: String, uni: String){
   val curso: Int = cur
   val carrera: String = car
   val universidad: String = uni
-
-  def getNombre(): String = {
-    return nombre
-  }
-
-  def getCurso(): Int = {
-    return curso
-  }
-
-  def getCarrera(): String = {
-    return carrera
-  }
-
-  def getUniversidad(): String = {
-    return universidad
-  }
 }
