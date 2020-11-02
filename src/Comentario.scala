fa1400e7b8e7fa126e148f4ee158d8027cb3de1d
María Jesús López Salmerón <mjls130598@gmail.com>
2020-11-02 17:52:06 +0100

    Modificado la clase Comentario

--- src/Comentario.scala
+++ src/Comentario.scala
@@ -5,16 +5,4 @@ class Comentario(coment: String, user: Usuario, apunt : Apunte){
   val comentario: String = coment
   val usuario: String = user
   val apunte: String = apunt
-
-  def getComentario(): String = {
-    return comentario
-  }
-
-  def getUsuario(): Usuario = {
-    return usuario
-  }
-
-  def getApunte(): Apunte = {
-    return apunte
-  }
 }
