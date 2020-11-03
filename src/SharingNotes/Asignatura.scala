695c82421158abfe88e3dcf3fd8c099e1502993a
María Jesús López Salmerón <mjls130598@gmail.com>
2020-11-03 18:26:33 +0100

    Añadido identificador a la clase Asignatura Close #23

--- src/SharingNotes/Asignatura.scala
+++ src/SharingNotes/Asignatura.scala
@@ -1,6 +1,8 @@
 package SharingNotes
 
-class Asignatura (nom: String, cur: Int, car: String, uni: String){
+class Asignatura (id: String, nom: String, cur: Int, car: String, uni: String){
+  
+  val identificador: String = id
   val nombre: String = nom
   val curso: Int = cur
   val carrera: String = car
