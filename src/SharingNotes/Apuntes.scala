6a6f1737878102f4e3652011f55b182633e74d57
María Jesús López Salmerón <mjls130598@gmail.com>
2020-11-03 18:23:49 +0100

    Añadidos atributos identificador y ubicación en la clase apunte Close #21

--- src/SharingNotes/Apuntes.scala
+++ src/SharingNotes/Apuntes.scala
@@ -1,7 +1,9 @@
 package SharingNotes
 
-class Apunte(nom: String, asig: Asignatura, us: Usuario){
+class Apunte(id: String, url: String, nom: String, asig: Asignatura, us: Usuario){
 
+  val identificador: String = id 
+  val ubicacion: String = url
   val nombre:String = nom
   val asignatura: Asignatura = asig
   val usuario: Usuario = us
