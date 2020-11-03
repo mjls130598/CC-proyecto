bc7fd7001c1df74dee69b8aeab2d93977d86e03e
María Jesús López Salmerón <mjls130598@gmail.com>
2020-11-03 18:28:44 +0100

    Añadido identificador en la clase Comentario Close #25

--- src/SharingNotes/Comentario.scala
+++ src/SharingNotes/Comentario.scala
@@ -1,7 +1,8 @@
 package SharingNotes
 
-class Comentario(coment: String, user: Usuario, apunt : Apunte){
+class Comentario(id: String, coment: String, user: Usuario, apunt : Apunte){
 
+  val identificador: String = id
   val comentario: String = coment
   val usuario: Usuario = user
   val apunte: Apunte = apunt
