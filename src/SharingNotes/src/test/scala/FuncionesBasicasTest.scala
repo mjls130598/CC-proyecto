9dfd3d66495e884431039f2bb6173b4f8ce269ee
María Jesús López Salmerón <mjls130598@gmail.com>
2020-11-08 12:49:43 +0100

    Realiza el test sobre la inserción de una asignatura #18 (Parte #28)

--- src/SharingNotes/src/test/scala/FuncionesBasicasTest.scala
+++ src/SharingNotes/src/test/scala/FuncionesBasicasTest.scala
@@ -4,14 +4,37 @@ import org.scalatest.FunSuite
 class FuncionesBasicasTest extends FunSuite {
 
   val sharing = new SharingNotes()
-  sharing.aniadirUsuario(new Usuario("María Jesús", "mjls130598@gmail.com", "MUII", "Granada"))
 
-  // Prueba de que se ha insertado un nuevo usuario al sistema
-  test("Insertado nuevo usuario") {
-    assertResult(1){
+  val usuario = new Usuario("María Jesús", "mjls130598@gmail.com", "MUII", "Granada")
+  val admin = new Administrador()
+
+  // Prueba de que se ha insertado los dos usuarios anteriores al sistema
+
+  sharing.aniadirUsuario(usuario)
+  sharing.aniadirUsuario(admin)
+
+  test("Insertado nuevos usuarios correctamente") {
+    assertResult(2){
       sharing.usuarios.size
     }
   }
 
-  
+  // Prueba de que se ha añadido una asignatura correctamente
+
+  sharing.aniadirAsignatura("CC", "1º", "MUII", "Granada", admin)
+
+  test("Insertada nueva asignatura correctamente"){
+    assertResult(1){
+      sharing.asignaturas.size
+    }
+  }
+
+  // Prueba de que no se ha añadido una asignatura
+
+  sharing.aniadirAsignatura("TID", "1º", "MUII", "Granada", usuario)
+  test("No insertada en el sistema"){
+    assertResult(1){
+      sharing.asignaturas.size
+    }
+  }
 }
