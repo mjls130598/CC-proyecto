41a06e432bd10217f4a9f656364ce5e81c2f9f4c
María Jesús López Salmerón <mjls130598@gmail.com>
2020-11-08 13:28:23 +0100

    Realiza el test sobre la inserción de comentarios #13 (Parte #28)

--- src/SharingNotes/src/test/scala/FuncionesBasicasTest.scala
+++ src/SharingNotes/src/test/scala/FuncionesBasicasTest.scala
@@ -8,7 +8,7 @@ class FuncionesBasicasTest extends FunSuite {
   val usuario = new Usuario("María Jesús", "mjls130598@gmail.com", "MUII", "Granada")
   val admin = new Administrador()
 
-  // Prueba de que se ha insertado los dos usuarios anteriores al sistema
+  // Comprueba de que se ha insertado los dos usuarios anteriores al sistema
 
   sharing.aniadirUsuario(usuario)
   sharing.aniadirUsuario(admin)
@@ -19,7 +19,7 @@ class FuncionesBasicasTest extends FunSuite {
     }
   }
 
-  // Prueba de que se ha añadido una asignatura correctamente
+  // Comprueba de que se ha añadido una asignatura correctamente
 
   sharing.aniadirAsignatura("CC", "1º", "MUII", "Granada", admin)
 
@@ -29,7 +29,7 @@ class FuncionesBasicasTest extends FunSuite {
     }
   }
 
-  // Prueba de que no se ha añadido una asignatura
+  // Comprueba de que no se ha añadido una asignatura
 
   sharing.aniadirAsignatura("TID", "1º", "MUII", "Granada", usuario)
   test("No insertada en el sistema"){
@@ -38,17 +38,24 @@ class FuncionesBasicasTest extends FunSuite {
     }
   }
 
-  // Prueba de que se ha añadido un nuevo apunte
+  // Comprueba de que se ha añadido un nuevo apunte
 
   sharing.aniadirApunte("CC.pdf", "Apunte de CC", sharing.asignaturas("ASIG1"), usuario)
   test("Insertado nuevo apunte correctamente"){
     assert(sharing.apuntes.keys.exists(_ == "APUN1"))
   }
 
-  // Prueba de que se ha añadido el apunte correcto
+  // Comprueba de que se ha añadido el apunte correcto
 
   sharing.aniadirApunte("CC.doc", "Apunte de CC", sharing.asignaturas("ASIG1"), usuario)
   test("No insertado apunte con un formato erróneo"){
     assert(sharing.apuntes.keys.exists(_ != "APUN2"))
+  }
+
+  // Comprueba de que se ha añadido un comentario correctamente
+
+  sharing.aniadirComentario("Esto es un comentario cualquiera", sharing.apuntes("APUN1"), usuario)
+  test("Insertado nuevo comentario correctamente"){
+    assert(sharing.comentarios.values.exists(_.identificador == "COM1"))
   }
 }
